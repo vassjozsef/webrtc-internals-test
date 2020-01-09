5412dce6f2806e455d65009df4468e29e374b38f
Jozsef Vass <jozsef@discordapp.com>
2020-01-09 10:48:57 -0800

    use 0.0.5, set color and time format

--- app/main.js
+++ app/main.js
@@ -2,12 +2,15 @@ const {TimelineDataSeries, TimelineGraphView} = require('webrtc-internals');
 
 const graph = document.getElementById('graph')
 const graphView = new TimelineGraphView(graph);
+graphView.backgroundColor = 'White';
+graphView.textColor = 'Black';
+graphView.gridColor = 'Black';
+graphView.timeOptions = {timeStyle: 'short'};
 
 const dataSeries = new TimelineDataSeries();
 graphView.addDataSeries(dataSeries);
 
 setInterval(() => {
-  const timestamp = Date.now();
-  dataSeries.addPoint(timestamp, Math.random());
+  dataSeries.addPoint(Date.now(), Math.random());
   graphView.updateEndDate();
 }, 1000);
